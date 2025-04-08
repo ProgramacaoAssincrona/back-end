250854b9da68d5868a769a204ab354e9df3feaad
enzotabuchi <124629400+TabuchiEnzo@users.noreply.github.com>
2025-04-08 11:07:28 -0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './api/products/products.module';
 import { CategoriesModule } from './api/categories/categories.module'
-import { CartModule } from './api/cart/cart.module';
 
 @Module({
   imports: [
     ProductsModule, 
-    CategoriesModule,
-    CartModule,
+    CategoriesModule
   ],
   controllers: [AppController],
   providers: [AppService],
